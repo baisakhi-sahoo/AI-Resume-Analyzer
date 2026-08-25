125c2215cee8a5a594392f989b3d6a09aeb0f8c0
Baisakhi Sahoo <sahoobaisakhi182@gmail.com>
2026-08-25 22:39:20 +0530

    Add initial Maven project and servlet

---
+++ src/main/java/org/example/ResumeServlet.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/analyze")
+public class ResumeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Resume Analyzer</h1>");
+        out.println("<p>Servlet received your request successfully!</p>");
+    }
+}
